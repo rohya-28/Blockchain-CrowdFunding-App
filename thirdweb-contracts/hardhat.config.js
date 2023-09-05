b47e578b9e27de670c1b911ea99a5a953484a0c0
rohya-28 <rohitarsude28@gmail.com>
2023-09-05 13:35:11 +0530

    added css framework -tailwind

--- thirdweb-contracts/hardhat.config.js
+++ thirdweb-contracts/hardhat.config.js
@@ -33,10 +33,10 @@ module.exports = {
   },
   solidity: {
     version: "0.8.17",
-    defaultNetwork: 'goerli',
+    defaultNetwork: 'polygon',
     networks: {
       hardhat: {},
-      goerli : {
+      polygon : {
         url : 'https://rpc.ankr.com/polygon_mumbai',
         accounts: [`0x${process.env.PRIVATE_KEY}`]
       }
