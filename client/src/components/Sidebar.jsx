050b554af4159444fcde25e3286696ed029fc455
rohya-28 <rohitarsude28@gmail.com>
2023-09-05 21:20:29 +0530

    resposive bug issue

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -42,6 +42,8 @@ const navigate = useNavigate();
                     />
                 ))}
             </div>
+
+            <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
         </div>
     </div>
   )
