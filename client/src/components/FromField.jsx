53787f7d38ffc352ff2c65bf9964f5bd3f033110
rohya-28 <rohitarsude28@gmail.com>
2023-09-07 19:25:24 +0530

    new pages added

---
+++ client/src/components/FromField.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const FromField = ({ LabelName, PlaceHolder, inputType, isTextArea, Value, handleChange }) => {
+  return (
+    <label className='flex-1 w-full flex flex-col'>
+        {LabelName && (
+        <span className='font-epilogue font-medium text-[14px] leading-[22px] text-[#808191] mb-[10px]'>
+            {LabelName}
+        </span>
+        )}
+        {isTextArea ? (
+            <textarea 
+            required 
+            value={Value}
+            onChange={handleChange}
+            rows={10}
+            placeholder={PlaceHolder}
+            className='py-[15px] sm:px-[25px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent
+            font-epilogue text-white text-[14px] placeholder:text-[#4b5264] rounded-[10px] sm:min-w-[300px]'
+            />
+        ) :
+        (
+         <input 
+         required 
+         value={Value}
+         onChange={handleChange}
+         type={inputType}
+         step='0.1'
+         placeholder={PlaceHolder}
+         className='py-[15px] sm:px-[25px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent
+         font-epilogue text-white text-[14px] placeholder:text-[#4b5264] rounded-[10px] sm:min-w-[300px]'
+         /> 
+        )
+        }
+    </label>
+  )
+}
+
+export default FromField
